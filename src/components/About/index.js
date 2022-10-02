148f1decce24fbce85422a43f226843e9e9160f0
babaphillips <amandavphillips@icloud.com>
2022-10-02 11:10:44 -0400

    Adding profileImage

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,11 @@
 import React from "react";
+import profileImage from "../../assets/IMG_1454.JPG";
 
 function About() {
   return (
     <section className="my-5">
       <div className="my-2">
-        <img class="me" alt="profile" img src="././assets/IMG_1454.JPG" />
+        <img class="me" alt="profile" img src={profileImage} />
         <p>
           Hello there! My name is Amanda Philips, and I am thrilled to be in the
           beginning phase of my career as a full stack web developer. I am a
